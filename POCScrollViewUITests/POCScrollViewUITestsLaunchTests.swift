f6d6f9d0af93299658f2b84474d33baee3854c08
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2022-11-22 09:07:51 -0300

    Initial Commit

---
+++ POCScrollViewUITests/POCScrollViewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  POCScrollViewUITestsLaunchTests.swift
+//  POCScrollViewUITests
+//
+//  Created by Lucas Barbosa de Oliveira on 22/11/22.
+//
+
+import XCTest
+
+final class POCScrollViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
